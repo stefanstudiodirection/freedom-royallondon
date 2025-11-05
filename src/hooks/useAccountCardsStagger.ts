a3c964741376884a478dc75c2e6a3ac93b8f0830
stefanstudiodirection <stefan.radovic@studiodirection.com>
2025-11-05 09:34:43 +0100

    gsap anim

--- src/hooks/useAccountCardsStagger.ts
+++ src/hooks/useAccountCardsStagger.ts
@@ -2,26 +2,28 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { RefObject } from "react";
 
-export const useAccountCardsStagger = (containerRef: RefObject<HTMLElement>) => {
-  useGSAP(
-    () => {
-      const cards = containerRef.current?.querySelectorAll(".account-card");
-      
-      if (!cards || cards.length === 0) return;
+export const useAccountCardsStagger = (
+	containerRef: RefObject<HTMLElement>
+) => {
+	useGSAP(
+		() => {
+			const cards = containerRef.current?.querySelectorAll(".account-card");
 
-      gsap.fromTo(
-        cards,
-        {
-          y: "calc(-100% - 32px)",
-        },
-        {
-          y: 0,
-          duration: 0.6,
-          ease: "linear",
-          stagger: 0.15,
-        }
-      );
-    },
-    { scope: containerRef }
-  );
+			if (!cards || cards.length === 0) return;
+
+			// Prvo odmah postavi poziciju
+			gsap.set(cards, {
+				y: "calc(-100% - 32px)",
+			});
+
+			// Pa onda animiraj
+			gsap.to(cards, {
+				y: 0,
+				duration: 0.6,
+				ease: "power2.out",
+				stagger: 0.15,
+			});
+		},
+		{ scope: containerRef }
+	);
 };
